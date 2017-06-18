bb9077af883974238f15daa681b74ef7c7b35d0b
Marcel Klammer <m.klammer@tastenkunst.com>
2017-06-18 16:53:11 +0200

    v4.0.0
    + Changed: Useful links

--- js/utils/BRFv4DownloadChooser.js
+++ js/utils/BRFv4DownloadChooser.js
@@ -8,15 +8,16 @@
 		QuickSettings.useExtStyleSheet();
 
 		brfv4Example.gui.downloadChooser = QuickSettings.create(
-			2, 270, "Download trial SDKs", brfv4Example.dom.createDiv("_settingsRight"))
+			2, 270, "Useful links", brfv4Example.dom.createDiv("_settingsRight"))
 			.setWidth(250)
-			.addHTML("Info", "The BRFv4 example packages are available on our Github page:<br><br>" +
-				"<a href='https://github.com/Tastenkunst/' target='_blank'>Github</a><br/><br/>").hideTitle("Info")
-			.addHTML("Other Links", "Other useful links:<br><br>" +
+			.addHTML("Github", "The BRFv4 example packages are available on our Github page:<br><br>" +
+				"<a href='https://github.com/Tastenkunst/' target='_blank'>Github</a><br/>" +
 				"<a href='https://tastenkunst.github.io/brfv4_docs/what_can_i_do_with_it.html' target='_blank'>What can I do with it?</a><br/>" +
-				// "<a href='https://www.beyond-reality-face.com' target='_blank'>Website</a><br/>" +
-				// "<a href='https://www.facebook.com/BeyondRealityFace' target='_blank'>Facebook</a><br/>" +
+				"<a href='https://tastenkunst.github.io/brfv4_docs/' target='_blank'>Docs / API reference</a><br/><br/>"
+			)
+			.addHTML("Contact", "" +
+				"<a href='http://www.tastenkunst.com/#/contact' target='_blank'>Email us for commercial license</a><br/>" +
 				"<a href='https://twitter.com/tastenkunst' target='_blank'>Twitter</a><br/><br/>"
-			).hideTitle("Other Links")
+			)
 		}
 })();
